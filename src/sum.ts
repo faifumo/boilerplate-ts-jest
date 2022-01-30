1bc0e6326b7ecc86396eabf5531d958d34f33a62
Queli Coto <quelicm@gmail.com>
2022-01-30 09:17:13 +0100

    [master]: ⚒️ Add ts-node, scripts and tsconfig

--- src/sum.ts
+++ src/sum.ts
@@ -1,3 +1,8 @@
 type Sum = (a: number, b: number) => number;
 
+// As interface definition
+// interface Sum {
+//     (a: number, b: number) : number
+// }
+
 export const sum: Sum = (a, b) => a + b;
