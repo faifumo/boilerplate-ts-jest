02e957ec3c50dae7f1594b7baa818920d0da9fb8
Queli Coto <quelicm@gmail.com>
2023-06-21 12:47:20 +0200

    [test-coverage-gh-action]: Increase fail coberture

--- src/math.ts
+++ src/math.ts
@@ -5,8 +5,23 @@ type Sum = (a: number, b: number) => number;
 //     (a: number, b: number) : number
 // }
 
+enum OPERATION {
+    'SUM' = 'sum',
+    'SUBTRACT' = 'sum',
+    'MULTIPLY' = 'multiply',
+}
+
 export const sum: Sum = (a, b) => a + b;
 
 export const multiply = (a:number, b:number): number => a * b;
 
 export const subtract = (a:number, b:number): number => a - b;
+
+export const failCoverage = (a:number, b:number, operation: string): number => {
+
+  if (operation === OPERATION.SUM) {
+    return sum(a,b);
+  }
+  
+  return 0;
+};
