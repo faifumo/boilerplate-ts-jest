b50679be535b5fbd1c06d2194b9d0bf478ebcad8
Queli Coto <quelicm@gmail.com>
2023-06-21 13:57:59 +0200

    [FTB-9-b]: ⚒️ re-run code climate

--- src/math.ts
+++ src/math.ts
@@ -18,14 +18,14 @@ export const multiply = (a:number, b:number): number => a * b;
 export const subtract = (a:number, b:number): number => a - b;
 
 export const failCoverage = (a:number, b:number, operation: string): number => {
-  
   if (operation === OPERATION.SUM) {
     return a + b;
   } else if (operation === OPERATION.SUBTRACT) {
-    return subtract(a,b);
+    return a - b;
   } else {
     return a + b;
   } 
+  
 
   
   
